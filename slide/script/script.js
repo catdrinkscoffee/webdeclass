3f52db6d5e331b3f30179a4bd296cafafb5c315c
catdrinkscoffee <catdrinkscoffee@gmail.com>
2022-03-17 07:55:30 +0900

    slide

--- slide/script/script.js
+++ slide/script/script.js
@@ -9,39 +9,47 @@ $(document).ready(function () {
     $(".nav-main").mouseleave(function () {
         $(this).children(".nav-sub-container").stop().slideUp();
     });
-    //    slider - fadein, fadeout 버전
-    var slideIndex_1 = 0, slideLen_1 = 2;
+
+    //  slider - fadein, fadeout 버전
+    var slideIndex_1 = 0,
+        slideLen_1 = 2;
     window.setInterval(function () {
-        if (slideIndex_1 === slideLen_1) {
-            slideIndex_1 = 0;
+        if (slideIndex_1 < slideLen_1) {
+            slideIndex_1++;
         } else {
-            slideIndex_1 = slideIndex_1 + 1;
+            slideIndex_1 = 0;
         }
         $(".slide-1>img").fadeOut();
         $(".slide-1>img").eq(slideIndex_1).fadeIn();
-    }, 2000);
+    }, 3000);
 
-    //    slider - directional slide 버전 - 좌우
-    var slideIndex_2 = 0, slideLen_2 = 3;
+    //  slider - directional slide 버전 - 좌우
+    var slideIndex_2 = 0,
+        slideLen_2 = 2;
     window.setInterval(function () {
-       if (slideIndex_2 < slideLen_2 - 1) {
+        if (slideIndex_2 < slideLen_2) {
             slideIndex_2++;
         } else {
             slideIndex_2 = 0;
         }
-        var slidePosition = slideIndex_2 * (-1200)+"px";
-        $(".slide-2").animate({left:slidePosition}, "slow");
+        var slidePosition = slideIndex_2 * (-1200) + "px";
+        $(".slide-2").animate({
+            left: slidePosition
+        }, "slow");
     }, 3000);
-    
-    //    slider - directional slide 버전 - 상하
-    var slideIndex_3 = 0, slideLen_3 = 3;
+
+    //  slider - directional slide 버전 - 상하
+    var slideIndex_3 = 0,
+        slideLen_3 = 2;
     window.setInterval(function () {
-       if (slideIndex_3 < slideLen_3 - 1) {
+        if (slideIndex_3 < slideLen_3) {
             slideIndex_3++;
         } else {
             slideIndex_3 = 0;
         }
-        var slidePosition = slideIndex_3* (-300)+"px";
-        $(".slide-3").animate({top:slidePosition}, "slow");
+        var slidePosition = slideIndex_3 * (-300) + "px";
+        $(".slide-3").animate({
+            top: slidePosition
+        }, "slow");
     }, 3000);
 });
